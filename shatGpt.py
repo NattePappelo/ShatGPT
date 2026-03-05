433a274055768a37133033b70ab1cb1907b91571
NattePappelo <axel.appelo@gmail.com>
2026-03-05 14:18:24 +0200

    Refactor shatGpt.py with improved comments
    Updated comments and preserved functionality of shatAPI.

--- shatGpt.py
+++ shatGpt.py
@@ -1,5 +1,9 @@
+# Detta är en "Ain't Inteligent" (AI) vid Namn "ShatGPT" av Natte     2024 - 05.03.2026
 
-def shatAPI(x):
+# shatAPI är ett sätt för vem som hällst att kontakta ShatGPT via python kod,
+# så vem som hällst kan enkelt tillämpa ShatGPT till sina egna avancerade projekt.
+
+def shatAPI(x):    
 	if "!" in x:
 		return "ÅHÅ"
 
@@ -18,4 +22,5 @@ if __name__ == "__main__":
 	print("\n\nVälkommen till ShatGPT 0.1.1 alfa \n\n")
 
 	while 1:
+
 		print("\n   "+ shatAPI(input(":  ")) +"\n")
